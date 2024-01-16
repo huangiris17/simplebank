ef6ca81d34d6a33c55fe0b881c8f8219fe11cf4c
Iris <i5052637@gmail.com>
2024-01-15 22:00:43 -0500

    add email sending feature

---
+++ mail/sender_test.go
@@ -0,0 +1,25 @@
+package mail
+
+import (
+	"testing"
+
+	"github.com/huangiris17/simplebank/util"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSendEmailWithGmail(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+	config, err := util.LoadConfig("..")
+	require.NoError(t, err)
+
+	sender := NewGmailSender(config.EmailSenderName, config.EmailSenderAddress, config.EmailSenderPassword)
+	subject := "A test email"
+	content := `<h1>Hello Worls</h1>`
+	to := []string{"techschool@gmail.com"}
+	attachFiles := []string{"../README.md"}
+
+	err = sender.SendEmail(subject, content, to, nil, nil, attachFiles)
+	require.NoError(t, err)
+}
--- util/config.go
+++ util/config.go
@@ -14,6 +14,9 @@ type Config struct {
 	GRPCServerAddress    string        `mapstructure:"GRPC_SERVER_ADDRESS"`
 	RedisAddress         string        `mapstructure:"REDIS_ADDRESS"`
 	TokenSymmetricKey    string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
+	EmailSenderName      string        `mapstructure:"EMAIL_SENDER_NAME"`
+	EmailSenderAddress   string        `mapstructure:"EMAIL_SENDER_ADDRESS"`
+	EmailSenderPassword  string        `mapstructure:"EMAIL_SENDER_PASSWORD"`
 	AccessTokenDuration  time.Duration `mapstructure:"ACCESS_tOKEN_DURATION"`
 	RefreshTokenDuration time.Duration `mapstructure:"REFRESH_TOKEN_DURATION"`
 }
